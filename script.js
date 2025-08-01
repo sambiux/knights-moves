17f6699fe0b52e0abf1ff76fca48949195d2d2bf
samuel daza <sddaza.9@gmail.com>
2025-07-31 19:24:51 -0500

    script controller

---
+++ script.js
@@ -0,0 +1,8 @@
+import { knightMoves } from "./knightsMoves.js";
+
+
+knightMoves([0, 0], [7, 7]);
+knightMoves([1, 2], [3, 3]);
+knightMoves([3, 3], [5, 1]);
+knightMoves([1, 1], [7, 6]);
+knightMoves([2, 3], [4, 7]);
